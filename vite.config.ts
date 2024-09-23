94e0237dd3060cbbb003871e22a39d8410da8521
TANU WIJAYA <92568646+txnu@users.noreply.github.com>
2024-09-24 03:32:42 +0700

    perubahan terbaru

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/LandingPage-KedaiProgrammer/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
